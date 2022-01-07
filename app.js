1e51557a6ed94f7545b7e0a40a13512d8400d7e3
Sidney de Sousa <esp.sousa@gmail.com>
2022-01-07 10:22:40 +0200

    adding styling and static files

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const app = express()
 const adminRoutes = require('./routes/admin')
 const mainRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false})) // this is used to parse the body content
+app.use(express.static(path.join(__dirname, 'public'))) // grant access to public files
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminRoutes) // route group
 app.use(mainRoutes)
 
 // use works for all HTTP methods express
