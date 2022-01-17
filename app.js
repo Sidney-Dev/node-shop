7fa6a69705da99dcc9133b045f741c77b4fcdcf3
Sidney de Sousa <esp.sousa@gmail.com>
2022-01-17 19:27:38 +0200

    basic setup with pug

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const adminRoutes = require('./routes/admin')
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+const adminData = require('./routes/admin')
 const mainRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false})) // this is used to parse the body content
 app.use(express.static(path.join(__dirname, 'public'))) // grant access to public files
 
-app.use('/admin', adminRoutes) // route group
+app.use('/admin', adminData.routes) // route group
 app.use(mainRoutes)
 
 // use works for all HTTP methods express
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', { pageTitle: "Page not found"})
 })
 
 app.listen(3000)
