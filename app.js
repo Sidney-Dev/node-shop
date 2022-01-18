7d299b3924fe4ff2d09f44c9bf9b692c5c46c285
Sidney de Sousa <esp.sousa@gmail.com>
2022-01-18 06:34:40 +0200

    view engine cleanup

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const expressHbs = require('express-handlebars')
 
 const app = express()
 
