6be991acdf67771d09106cd69e97fcdc5a319034
Sidney de Sousa <esp.sousa@gmail.com>
2022-01-18 05:54:42 +0200

    handlebars and ejs

--- routes/shop.js
+++ routes/shop.js
@@ -8,10 +8,15 @@ const adminData = require("./admin")
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    const products = adminData.products
-    res.render('shop', {prods: products, pageTitle: "Shop", path: '/'})
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-    // OLD res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
-}) 
-
-module.exports = router
+    const products = adminData.products;
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+  
+  module.exports = router;
